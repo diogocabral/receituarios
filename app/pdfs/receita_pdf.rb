8b63d074bb88f522e6040c7e3e0088232c025c54
Diogo Cabral de Almeida <diogo.cabral@gmail.com>
2016-06-08 22:25:44 -0300

    Creepy pdf hahaha

--- app/pdfs/receita_pdf.rb
+++ app/pdfs/receita_pdf.rb
@@ -5,32 +5,49 @@ class ReceitaPdf < Prawn::Document
 
     @receita = receita
 
-    # bounding_box [bounds.left, bounds.bottom + 40], width: bounds.width, height: 40 do
+    # bounding_box [bounds.left, bounds.top + 40], width: bounds.width, height: 40 do
     	add_header
-    	move_down 50
-    	add_body(receita)
-    	move_down 50
-		add_footer
+      move_down 10
     # end
+
+    add_body(receita)
+
+    bounding_box [bounds.left, bounds.bottom + 40], width: bounds.width, height: 40 do
+      add_footer
+    end
   end
 
   private
 
   	def add_header
-  		text "RECEITUÁRIO", align: :center
+  		text "Receituário", align: :center, size: 20
+      stroke_horizontal_rule
   	end
 
   	def add_body(receita)
-		@receita.itens.each do |item|
-		  text item.medicamento.nome + "---------------------------" + " " + item.unidade_medida.nome, align: :left
-		  text item.instrucoes_de_uso, align: :left
-		  text "Sugestão de horário:" + item.sugestao_horario, align: :left
-		end
+
+      text "Nome: Diogo Cabral de Almeida", :size => 10
+      move_down 20
+  		@receita.itens.each do |item|
+        medicamento_size = (item.medicamento.nome + item.quantidade.to_s + item.unidade_medida.nome).size
+
+        asd = "-" * (118 - medicamento_size)
+
+  		  text "#{item.medicamento.nome} #{asd} #{item.quantidade} #{item.unidade_medida.nome}", :size => 10
+  		  text "Uso: " + item.instrucoes_de_uso, :size => 8
+
+        if item.sugestao_horario?
+  		    text "Sugestão de horário: " + item.sugestao_horario, :size => 8
+        end
+        move_down 20
+  		end
   	end
 
     def add_footer
-      text "Rua Barão de Maceió, 288 – CEP: 57.020-360 – Centro – Maceió – Alagoas – Brasil", align: :center
-      text "CNPJ: 12.307.187/0001-50 – Inscrição Estadual: 24.054.180-4", align: :center
+      stroke_horizontal_rule
+      move_down 5
+      text "Rua Barão de Maceió, 288 – CEP: 57.020-360 – Centro – Maceió – Alagoas – Brasil", align: :center, :size => 8
+      text "CNPJ: 12.307.187/0001-50 – Inscrição Estadual: 24.054.180-4", align: :center, :size => 8
     end
 
 end
